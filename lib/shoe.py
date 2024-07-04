e5016b3a39cb52b3f9cc45809eef639b159f6240
Salyiel <162126755+Salyiel@users.noreply.github.com>
2024-07-04 17:22:47 +0300

    Complete lab assessment

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,36 @@
 #!/usr/bin/env python3
+import io
+import sys
 
 class Shoe:
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "Old"
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @brand.setter
+    def brand(self, value):
+        self._brand = value
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+            sys.stdout.flush()  # Ensure the output is flushed immediately
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        sys.stdout.flush()  # Ensure the output is flushed immediately
+        self.condition = "New"
+
     pass
